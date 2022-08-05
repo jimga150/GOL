ed0fe0d775307da16ece43dfe359b66624fbc1d9
jimga150 <jimga150@users.noreply.github.com>
2022-08-05 18:51:49 -0400

    Add GOL block, wrapping the stepper, RAM, and getter

---
+++ GOL_chunks/GOL_chunks.srcs/sim_1/imports/Jim/GOL_block_tb.vhd
@@ -0,0 +1,177 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 08/05/2022 18:24:14
+-- Design Name: GOL_block_tb
+-- Module Name: GOL_block_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments: Created with VHDL Test Bench Template Generator
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.GOL_pkg.all;
+use work.bmp_pkg.all;
+
+entity GOL_block_tb is
+end GOL_block_tb;
+
+architecture Behavioral of GOL_block_tb is
+    
+    --Clocks
+    signal i_clk : std_logic := '0';
+    
+    --Resets
+    signal i_rst : std_logic := '1';
+    
+    --General inputs
+    signal i_chunk_x : unsigned(c_num_chunk_col_bits-1 downto 0) := (others => '0');
+    signal i_chunk_y : unsigned(c_num_chunk_col_bits-1 downto 0) := (others => '0');
+    signal i_do_frame : std_logic := '0';
+    
+    --Loopbacks
+    signal s_top_to_bottom_edge : std_logic_vector(c_num_cell_cols-1 downto 0);
+    signal s_bottom_to_top_edge : std_logic_vector(c_num_cell_cols-1 downto 0);
+    signal s_right_to_left_edge : std_logic_vector(c_num_cell_rows-1 downto 0);
+    signal s_left_to_right_edge : std_logic_vector(c_num_cell_rows-1 downto 0);
+    signal s_top_left_to_bottom_right_bit : std_logic;
+    signal s_top_right_to_bottom_left_bit : std_logic;
+    signal s_bottom_left_to_top_right_bit : std_logic;
+    signal s_bottom_right_to_top_left_bit : std_logic;
+    
+    --Outputs
+    signal o_chunk : t_chunk_type;
+    
+    --Clock Periods
+    constant i_clk_period : time := 10 ns;
+    
+begin
+    
+    UUT: entity work.GOL_block
+    port map(
+        i_clk => i_clk,
+        i_rst => i_rst,
+        i_chunk_x => i_chunk_x,
+        i_chunk_y => i_chunk_y,
+        o_chunk => o_chunk,
+        i_do_frame => i_do_frame,
+        i_top_edge => s_bottom_to_top_edge,
+        i_bottom_edge => s_top_to_bottom_edge,
+        i_right_edge => s_left_to_right_edge,
+        i_left_edge => s_right_to_left_edge,
+        i_top_left_bit => s_bottom_right_to_top_left_bit,
+        i_top_right_bit => s_bottom_left_to_top_right_bit,
+        i_bottom_left_bit => s_top_right_to_bottom_left_bit,
+        i_bottom_right_bit => s_top_left_to_bottom_right_bit,
+        o_top_edge => s_top_to_bottom_edge,
+        o_bottom_edge => s_bottom_to_top_edge,
+        o_right_edge => s_right_to_left_edge,
+        o_left_edge => s_left_to_right_edge,
+        o_top_left_bit => s_top_left_to_bottom_right_bit,
+        o_top_right_bit => s_top_right_to_bottom_left_bit,
+        o_bottom_left_bit => s_bottom_left_to_top_right_bit,
+        o_bottom_right_bit => s_bottom_right_to_top_left_bit
+    );
+    
+    --Clock Drivers
+    i_clk <= not i_clk after i_clk_period/2;
+    
+    stim_proc: process is begin
+        
+        wait for i_clk_period;
+        
+        i_rst <= '0';
+        
+        wait for i_clk_period;
+        
+        for i in 0 to 100 loop
+        
+            i_do_frame <= '1';
+            
+            wait for i_clk_period;
+            
+            i_do_frame <= '0';
+            
+            wait for i_clk_period;
+            
+            for r in 0 to c_num_chunk_rows - 1 loop
+                for c in 0 to c_num_chunk_cols - 1 loop
+                    i_chunk_x <= to_unsigned(c, i_chunk_x'length);
+                    i_chunk_y <= to_unsigned(c, i_chunk_x'length);
+                    wait for i_clk_period;
+                end loop;
+            end loop;
+            
+            wait for i_clk_period*7000;
+        
+        end loop;
+        
+        assert false report "End Simulation" severity failure;
+        
+        -- Not strictly necessary, but prevents process from looping 
+        -- if the above assert statement is removed
+        wait;
+        
+    end process;
+    
+    bmp_write_proc: process is
+        variable v_chunk : t_chunk_type;
+        variable v_cell_x, v_cell_y : natural;
+        variable v_bmp_ptr : bmp_ptr := new bmp;
+        variable v_bmp_is_init : boolean := false;
+        constant c_bmp_pix_0 : bmp_pix := (others => (others => '0'));
+        constant c_bmp_pix_1 : bmp_pix := (others => (others => '1'));
+        variable v_bmp_pix : bmp_pix;
+        variable v_step_num : integer := 1;
+    begin
+    
+        if (not v_bmp_is_init) then
+            v_bmp_ptr.meta.width := c_num_cell_cols;
+            v_bmp_ptr.meta.height := c_num_cell_rows;
+            v_bmp_is_init := true;
+        end if;
+        
+        wait until i_do_frame = '1';
+        
+        wait for i_clk_period*5;
+        
+        for r in 0 to c_num_chunk_rows - 1 loop
+            for c in 0 to c_num_chunk_cols - 1 loop
+            
+                v_chunk := o_chunk;
+                
+                for y in r*c_chunk_height to (r+1)*c_chunk_height - 1 loop
+                    v_cell_y := y mod c_chunk_height;
+                    for x in c*c_chunk_width to (c+1)*c_chunk_width - 1 loop
+                        v_cell_x := x mod c_chunk_width;
+                        v_bmp_pix := c_bmp_pix_0;
+                        if (v_chunk(v_cell_y)(v_cell_x) = '1') then 
+                            v_bmp_pix := c_bmp_pix_1;
+                        end if;
+                        bmp_set_pix(v_bmp_ptr, x, y, v_bmp_pix);
+                    end loop;
+                end loop;
+                
+                wait for i_clk_period;
+                
+            end loop;
+        end loop;
+        
+        bmp_save(v_bmp_ptr, "..\..\..\..\GOL_steps\GOL_step_" & integer'image(v_step_num) & ".bmp");
+        v_step_num := v_step_num + 1;
+        
+    end process;
+
+end Behavioral;
---
+++ GOL_chunks/GOL_chunks.srcs/sources_1/new/GOL_block.vhd
@@ -0,0 +1,136 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 08/05/2022 05:48:27 PM
+-- Design Name: 
+-- Module Name: GOL_block - Structural
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+use work.GOL_pkg.all;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity GOL_block is
+    port(
+        i_clk, i_rst : in std_logic;
+        
+        i_chunk_x : in unsigned(c_num_chunk_col_bits-1 downto 0);
+        i_chunk_y : in unsigned(c_num_chunk_col_bits-1 downto 0);
+        o_chunk : out t_chunk_type;
+        
+        i_do_frame: in std_logic;
+        
+        i_top_edge, i_bottom_edge : in std_logic_vector(c_num_cell_cols-1 downto 0) := (others => '0');
+        i_right_edge, i_left_edge : in std_logic_vector(c_num_cell_rows-1 downto 0) := (others => '0');
+        i_top_left_bit, i_top_right_bit, i_bottom_left_bit, i_bottom_right_bit : in std_logic := '0';
+        
+        o_top_edge, o_bottom_edge : out STD_LOGIC_VECTOR(c_num_cell_cols-1 downto 0);
+        o_right_edge, o_left_edge : out STD_LOGIC_VECTOR(c_num_cell_rows-1 downto 0);
+        o_top_left_bit, o_top_right_bit, o_bottom_left_bit, o_bottom_right_bit : out std_logic
+    );
+end GOL_block;
+
+architecture Structural of GOL_block is
+
+    signal s_current_state_msb : std_logic;
+    
+    signal s_bram_clka : std_logic;
+    signal s_bram_enaa, s_bram_wea : std_logic;
+    signal s_bram_addra : std_logic_vector(9 downto 0);
+    signal s_bram_wr_dataa : std_logic_vector(35 downto 0);
+    signal s_bram_rd_dataa : std_logic_vector(35 downto 0);
+    
+    signal s_bram_clkb : std_logic;
+    signal s_bram_enab, s_bram_web : std_logic;
+    signal s_bram_addrb : std_logic_vector(9 downto 0);
+    signal s_bram_wr_datab : std_logic_vector(35 downto 0);
+    signal s_bram_rd_datab : std_logic_vector(35 downto 0);
+
+begin
+
+    block_stepper_inst: entity work.GOL_block_stepper
+    port map(
+        i_clk => i_clk,
+        i_rst => i_rst,
+        i_do_frame => i_do_frame,
+        i_top_edge => i_top_edge,
+        i_bottom_edge => i_bottom_edge,
+        i_right_edge => i_right_edge,
+        i_left_edge => i_left_edge,
+        i_top_left_bit => i_top_left_bit,
+        i_top_right_bit => i_top_right_bit,
+        i_bottom_left_bit => i_bottom_left_bit,
+        i_bottom_right_bit => i_bottom_right_bit,
+        o_bram_clk => s_bram_clka,
+        o_bram_ena => s_bram_enaa,
+        o_bram_we => s_bram_wea,
+        o_bram_addr => s_bram_addra,
+        o_bram_wr_data => s_bram_wr_dataa,
+        i_bram_rd_data => s_bram_rd_dataa,
+        o_top_edge => o_top_edge,
+        o_bottom_edge => o_bottom_edge,
+        o_right_edge => o_right_edge,
+        o_left_edge => o_left_edge,
+        o_top_left_bit => o_top_left_bit,
+        o_top_right_bit => o_top_right_bit,
+        o_bottom_left_bit => o_bottom_left_bit,
+        o_bottom_right_bit => o_bottom_right_bit,
+        o_current_state_msb => s_current_state_msb
+    );
+    
+    bram_inst: entity work.bram_dp_36k
+    port map(
+        i_clka => s_bram_clka,
+        i_ena => s_bram_enaa,
+        i_wea => s_bram_wea,
+        i_addra => s_bram_addra,
+        i_dina => s_bram_wr_dataa,
+        o_douta => s_bram_rd_dataa,
+        i_clkb => s_bram_clkb,
+        i_enb => s_bram_enab,
+        i_web => s_bram_web,
+        i_addrb => s_bram_addrb,
+        i_dinb => s_bram_wr_datab,
+        o_doutb => s_bram_rd_datab
+    );
+    
+    chunk_reader_inst: entity work.GOL_chunk_getter
+    port map(
+        i_clk => i_clk,
+        i_chunk_x => i_chunk_x,
+        i_chunk_y => i_chunk_y,
+        i_curr_state_msb => s_current_state_msb,
+        o_chunk => o_chunk,
+        o_bram_clk => s_bram_clkb,
+        o_bram_ena => s_bram_enab,
+        o_bram_addr => s_bram_addrb,
+        i_bram_rd_data => s_bram_rd_datab
+    );
+    
+    s_bram_web <= '0';
+    s_bram_wr_datab <= (others => '0');
+
+end Structural;
